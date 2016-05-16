657ac23225c168bdefb1883b1771755f0e471995
codeamac02 <codeamac02@gmail.com>
2016-05-16 10:53:24 -0500

    Crea un método que devuelva en aleatorio un array

---
+++ loteria.rb
@@ -0,0 +1,14 @@
+def shuffle(array)
+ p array.shuffle
+end
+
+# Driver code:
+array = (1..15).to_a
+
+# Esto no deberá arrojar una excepción
+10.times do |variable|
+    raise "No esta revolviendo bien" if shuffle(array) == shuffle(array)
+end
+
+p true
+
